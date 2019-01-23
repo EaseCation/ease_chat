9c90c585f540164c73890928fb266dc22efa2b4c
luojia65 <me@luojia.cc>
2019-01-24 02:48:55 +0800

    update before class over

---
+++ src/easy_chat_nexus/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+
+c/hub:main
+c/hub:cw
+c/stage:10156
+f/intl:boybook
+f/chn:whateverid
+f/admin:server
+
+every channel(c) and flair(f) have a valid time span
+called 'valid until', in UTC time
+
+
+ */
