f8a0ca8b59a10298330282d4bccc0ff883cbfa3f
luojia65 <me@luojia.cc>
2019-02-14 14:44:14 +0800

    rename

--- src/easechat-record-rs/src/main.rs
+++ src/easechat-record-rs/src/main.rs
@@ -1,10 +1,19 @@
+#[macro_use]
+extern crate clap;
+
 use std::thread;
 use rusqlite::{NO_PARAMS, Connection};
-use clap::{App, load_yaml};
+use clap::App;
 
 fn main() { 
-    let yaml = load_yaml!("recorder-cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let matches = clap_app!(easechat_record =>
+        (version: "0.0.0")
+        (author: "Luo Jia <me@luojia.cc>")
+        (about: "Connect to easechat server and record messages on some channels")
+        (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
+        (@arg IP: +required "Sets the ip address of server")
+        (@arg PORT: -p "Sets the port of server, default to 6500")
+    ).get_matches();
     let conn = Connection::open_in_memory().unwrap();
     conn.execute(
         "CREATE TABLE IF NOT EXISTS msg (
