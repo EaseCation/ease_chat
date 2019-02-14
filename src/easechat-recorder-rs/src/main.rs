cf9ae81245e3a604711dbf8442ce9782da6296ae
luojia65 <me@luojia.cc>
2019-02-14 14:27:27 +0800

    recorder cli args

--- src/easechat-recorder-rs/src/main.rs
+++ src/easechat-recorder-rs/src/main.rs
@@ -1,5 +1,30 @@
-mod console;
+use std::thread;
+use rusqlite::{NO_PARAMS, Connection};
+use clap::{App, load_yaml};
 
 fn main() { 
-    
+    let yaml = load_yaml!("recorder-cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let conn = Connection::open_in_memory().unwrap();
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS msg (
+            id              INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            time            TEXT NOT NULL,
+            channel         TEXT NOT NULL,
+            text            TEXT NOT NULL
+        )",
+        NO_PARAMS,
+    ).unwrap();
+
+    // let url = "localhost:6500";
+
+    // thread::spawn(move || {
+    //     ws::connect(url, |out| {
+    //         out.send("Hello WebSocket").unwrap();
+    //         move |msg| {
+    //             println!("Got message: {}", msg);
+    //             out.close(CloseCode::Normal)
+    //         }
+    //     }).unwrap();
+    // });
 }
